867bdb84aa5d2ade4c66133c90e3cc28a672e229
Riswanto <wawan2072@gmail.com>
2020-05-22 09:31:21 +0700

    Updates

---
+++ precache-manifest.f615d2bdddd2fee7a9888aaa31b70724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5014da5966e84b17dd839f335a4c1681",
+    "url": "/get-app/index.html"
+  },
+  {
+    "revision": "cb749854819fc4a31bde",
+    "url": "/get-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fba9c6d888fd69143270",
+    "url": "/get-app/static/js/2.809795bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/get-app/static/js/2.809795bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb749854819fc4a31bde",
+    "url": "/get-app/static/js/main.f38903fe.chunk.js"
+  },
+  {
+    "revision": "9e7aa72365de4df882cd",
+    "url": "/get-app/static/js/runtime-main.8106d340.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/get-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
